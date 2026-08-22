57f847a8a7133d841fccaf12c293297c9756be9b
kenjiakahane <kenji.a123@icloud.com>
2026-08-23 07:34:24 +0900

    Create first garden journal screen

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <h1>My Garden 🌱</h1>
+
+      <textarea placeholder="Write about your day..." />
+
+      <button>Save</button>
+    </main>
+  );
+}
+
+export default App;
